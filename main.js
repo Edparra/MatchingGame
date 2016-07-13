869e48fb7d7d2d62ab663a08ac059b31139596e3
Edparra <Edprr4@gmail.com>
2016-07-13 17:54:03 -0400

    needs some work

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ var cardThree = "queen";
 
 var cardFour = "king";
 
+
+var cards = ['queen', 'queen', 'king', 'king'];
+
+var cardsInPlay = [];
+
  /* if (cardOne === cardThree) {
  
      alert("You found a match!");
@@ -22,11 +27,25 @@ var cardFour = "king";
 var createBoard = function () {
 
 // create for loop to make HTML element (div)
-  for (var i = 0; i < 4; i++) {
+  for (var i = 0; i < cards.length; i++) {
   	var cardElement = document.createElement('div');
   	cardElement.className = 'card';
   	board.appendChild(cardElement);
+  	cardElement.innerHTML = cards[i];
+  	cardElement.addEventListener('click', isTwoCards);
+  	
+//Need to go back in find where to put images
+  	//<img src="images/king.jpg" alt="King"/>
+  	//<img src="images/queen.jpg" alt="Queen"/>
   }
+}
+
+var isTwoCards = function () {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
 }
 
 createBoard();
